ba426c4cbd92285f7160881a64396f27acc5a590
jpfnice <jpfnice@gmail.com>
2026-09-18 11:30:13 +0200

    Add files via upload

--- exercise10_version1.py
+++ exercise10_version1.py
@@ -1,69 +1,83 @@
+
 """
-Write a Stack class using a composition mechanism (the Stack class will use internally the List class).
-The Stack class should offer the following facilities:
-    a push() method to add an element on top of the Stack
-    a pop() method to get and remove the element on top of the Stack
-    a peek() method to get the element on top of the Stack
-    a way to determine its size (its current number of elements),
-    a way to print easily the content of the Stack
-    a way to test if an element belongs to the stack or not
-    
-Name of the method to provide:
-    
-    push(self,element) -> None
-    pop(self) -> element
-    peek(self) -> element
-    __len__(self) -> int
-    __repr__(self) -> str
-    __contains__(self, element) -> bool
-    __init__(self,mSize) -> None
-    
-Name of the attributes to provide:
-    maxSize -> int
-    content -> list
-    
+Class Stack definition:
+    Attributes:
+        maxSize (an int)
+        content (a list)
+    Methods:
+        __init__()
+        push()
+        __repr__()
+        __len__()
+        pop()
+        peek()
+        isEmpty()
+        __eq__()
+
 """
 class Stack:
-    def push(self, element):
-        if len(self) < self.maxSize:
-            self.content.append(element)
-        else:
-            raise Exception("The maximum size is reached !!")
-    def pop(self):
-        if len(self) > 0:
-            return self.content.pop()
-        else:
-            raise Exception("The Stack is empty !!")
-    def peek(self) :
-        if len(self) > 0:
-            return self.content[-1]
-        else:
-            raise Exception("The Stack is empty !!")
-    def __len__(self) :
-        return len(self.content)
-    def __repr__(self) : # (3/12) [10,8,67]
-        return f"({len(self)}/{self.maxSize}) {self.content}"
-    def  __contains__(self, element) :
-        return element in self.content
-    def __init__(self, msize) :
-        if isinstance(msize, int) and msize > 0:
-            self.maxSize=msize
-        else:
-            raise Exception("Wrong size given!")
-        self.content=[] # <=> self.content=list()
-        
     
-s1=Stack(12) # 12 is the maxmimum size of the Stack s1
-s1.push(10)
-s1.push(8)
-s1.push(67)
-print(s1) # (3/12) [10,8,67]
-print("Size of the stack", len(s1)) # Size of the stack 3
+    def __init__(self,maxSZ):
+        if isinstance(maxSZ, int) and maxSZ> 0:
+            self.maxSize=maxSZ
+        else:
+            print("Wrong size given, 10 used instead !")
+            self.maxSize=10
+        self.content=[]
+        
+    def __repr__(self):
+        return f"({len(self.content)}/{self.maxSize}) {self.content}"
+    
+    def __len__(self):
+        return len(self.content)
+    
+    def __eq__(self, other):
+        return self.maxSize == other.maxSize and self.content == other.content
+    
+    def push(self, obj):
+        if len(self) >= self.maxSize:
+            print("Sorry: the stack is full!")
+        else:
+            self.content.append(obj)
+            
+    def pop(self):
+        if len(self) <= 0:
+            print("Sorry: the stack is empty!")
+            return None
+        else:
+            return self.content.pop(-1)
+        
+    def peek(self):
+        if len(self) <= 0:
+            print("Sorry: the stack is empty!")
+            return None
+        else:
+            return self.content[-1]
+        
+    def isEmpty(self):
+        return len(self) == 0
+        
+s1=Stack(10) # A Stack with a maximum size of 10 elements
+s1.push(20.3)
+s1.push(3.5)
+s1.push(4.5)
+print("S1 is", s1) # S1 is (3/10) [20.3, 3.5, 4.5]
+
+print("Current size of s1 is", len(s1)) # Current size of s1 is 3
 top=s1.pop()
-print(s1) # (2/12) [10,8]
-print("Size of the stack", len(s1)) # Size of the stack 2
-print(top) # 67
+print(top) # 4.5
+print("Current size of s1 is", len(s1)) # Current size of s1 is 2
 top=s1.peek()
-print(s1) # (2/12) [10,8]
-print("Size of the stack", len(s1)) # Size of the stack 2
-print(top) # 8
+print(top) # 3.5
+print("Current size of s1 is", len(s1)) # Current size of s1 is 2
+print("S1 is", s1)# S1 is (2/10) [20.3, 3.5]
+if s1.isEmpty():
+    print("S1 is empty")
+else:
+    print("S1 is not empty")
+    
+s2=Stack(10)
+print(s1==s2) # Should print False
+s2.push(20.3)
+s2.push(3.5)
+print(s1==s2) # Should print True

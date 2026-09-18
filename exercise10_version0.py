27dca91ba8de21d72430e7965a2b484b1a5373a7
jpfnice <jpfnice@gmail.com>
2026-09-18 11:05:56 +0200

    Add files via upload

--- exercise10_version0.py
+++ exercise10_version0.py
@@ -1,29 +1,42 @@
+
 """
-Write a Stack class using a composition mechanism (the Stack class will use internally the List class).
-The Stack class should offer the following facilities:
-    a push() method to add an element on top of the Stack
-    a pop() method to get and remove the element on top of the Stack
-    a peek() method to get the element on top of the Stack
-    a way to determine its size (its current number of elements),
-    a way to print easily the content of the Stack
-    a way to test if an element belongs to the stack or not
-    
+Class Stack definition:
+    Attributes:
+        maxSize (an int)
+        content (a list)
+    Methods:
+        __init__()
+        push()
+        __repr__()
+        __len__()
+        pop()
+        peek()
+        isEmpty()
+        __eq__()
+
 """
-class Stack:
-    pass #TO BE COMPLETED
-        
-    
-s1=Stack(12) # 12 is the maxmimum size of the Stack s1
-s1.push(10)
-s1.push(8)
-s1.push(67)
-print(s1) # (3/12) [10,8,67]
-print("Size of the stack", len(s1)) # Size of the stack 3
+
+s1=Stack(10) # A Stack with a maximum size of 10 elements
+s1.push(20.3)
+s1.push(3.5)
+s1.push(4.5)
+print("S1 is", s1) # S1 is (3/10) [20.3, 3.5, 4.5]
+
+print("Current size of s1 is", len(s1)) # Current size of s1 is 3
 top=s1.pop()
-print(s1) # (2/12) [10,8]
-print("Size of the stack", len(s1)) # Size of the stack 2
-print(top) # 67
+print(top) # 4.5
+print("Current size of s1 is", len(s1)) # Current size of s1 is 2
 top=s1.peek()
-print(s1) # (2/12) [10,8]
-print("Size of the stack", len(s1)) # Size of the stack 2
-print(top) # 8
+print(top) # 3.5
+print("Current size of s1 is", len(s1)) # Current size of s1 is 2
+print("S1 is", s1)# S1 is (2/10) [20.3, 3.5]
+if s1.isEmpty():
+    print("S1 is empty")
+else:
+    print("S1 is not empty")
+    
+s2=Stack(10)
+print(s1==s2) # Should print False
+s2.push(20.3)
+s2.push(3.5)
+print(s1==s2) # Should print True
